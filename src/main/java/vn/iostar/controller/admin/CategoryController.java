d419973914033ff749378cb9dc2e3c5b637615a7
TNAT <truongthu8628@gmail.com>
2024-10-30 22:32:17 +0700

    Add, Edit, Delete, Paging and Header(include Avatar) & Footer(include Information)

--- src/main/java/vn/iostar/controller/admin/CategoryController.java
+++ src/main/java/vn/iostar/controller/admin/CategoryController.java
@@ -2,18 +2,26 @@ package vn.iostar.controller.admin;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.validation.Valid;
@@ -84,5 +92,53 @@ public class CategoryController {
 		}
 		model.addAttribute("message", "Category is not exited!!!");
 		return new ModelAndView("forward:/admin/categories", model);
+	}
+	@GetMapping("delete/{categoryId}")
+	public ModelAndView delete(ModelMap model, @PathVariable("categoryId") int categoryId) {
+		categoryService.deleteById(categoryId);
+		model.addAttribute("message", "Category is deleted!!!!");
+		return new ModelAndView("forward:/admin/categories/searchpaginated", model);
+	}
+	@GetMapping("search")
+	public String search(ModelMap model, @RequestParam(name="name", required = false) String name) {
+		List<Category> list = null;
+		//Có nội dung truyền về không, name là tùy chon khi required = false
+		if(StringUtils.hasText(name)) {
+			list = categoryService.findByNameContaining(name);
+		} else {
+			list = categoryService.findAll();
+		}
+		model.addAttribute("categories",list);
+		return "admin/categories/search";
+	}
+	
+	@RequestMapping("searchpaginated")
+	public String search(ModelMap model, 
+			@RequestParam(name="name", required = false) String name,
+			@RequestParam("page") Optional<Integer> page,
+			@RequestParam("size") Optional<Integer> size) {
+		int count = (int) categoryService.count();
+		int currentPage = page.orElse(1);
+		int pageSize = size.orElse(3);
+		Pageable pageable = PageRequest.of(currentPage-1, pageSize, Sort.by("name"));
+		Page<Category> resultPage = null;
+		if(StringUtils.hasText(name)) {
+			resultPage = categoryService.findByNameContaining(name, pageable);
+			model.addAttribute("name", name);
+		} else {
+			resultPage = categoryService.findAll(pageable);
+		}
+		int totalPages = resultPage.getTotalPages();
+		if(totalPages > 0) {
+			int start = Math.max(1, currentPage-2);
+			int end = Math.min(currentPage+2, totalPages);
+			if(totalPages > count) {
+				if(end == totalPages) start = end - count;
+				else if (start == 1) end = start + count;
+			}
+			List<Integer> pageNumbers = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+			model.addAttribute("pageNumders", pageNumbers);
+		} model.addAttribute("categoryPage", resultPage);
+		return "admin/categories/searchpaginated";
 	}
 }
